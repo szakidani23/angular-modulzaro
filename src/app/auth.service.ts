e2f39b29a499b3295050fe3405849f61afb47d56
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-09-05 13:20:14 +0200

    AuthService: started, localstorage, api calling method

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  usersDB: any[] = JSON.parse(localStorage.getItem('usersDB') || '[]');
+  private apiUrl = 'https://siposm.hu/developerAPI/login';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  apiLogin(username: string, password: string): Observable<any> {
+    return this.http.post(this.apiUrl, { username, password }).pipe(
+      catchError((error) => {
+        console.log('API login failed: ', error);
+        throw error;
+      })
+    );
+  }
 }
