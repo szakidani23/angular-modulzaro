a7a4f07f908562f4d01823462e52e1de50d440e8
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-09-09 10:07:57 +0200

    Edit, DEL: Logic added in post-editor TS file

--- src/app/post-editor/post-editor.component.ts
+++ src/app/post-editor/post-editor.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post-editor',
   templateUrl: './post-editor.component.html',
-  styleUrl: './post-editor.component.scss'
+  styleUrls: ['./post-editor.component.scss'],
 })
-export class PostEditorComponent {
+export class PostEditorComponent implements OnInit {
+  @Input() post: any;
+  @Output() savePost = new EventEmitter<any>();
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSave() {
+    this.savePost.emit(this.post);
+  }
 }
