2fe82a191049849e8e467171e96b9fa230f2f232
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-09-09 09:58:22 +0200

    Edit, DEL: Post Editor comp. added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { PostEditorComponent } from './post-editor/post-editor.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HomeComponent],
+  declarations: [AppComponent, LoginComponent, HomeComponent, PostEditorComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/post-editor/post-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-editor',
+  templateUrl: './post-editor.component.html',
+  styleUrl: './post-editor.component.scss'
+})
+export class PostEditorComponent {
+
+}
