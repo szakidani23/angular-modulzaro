a85ddac54e84c1095372dd8b002c52b654ffaaf9
Daniel Szakaly <129371637+szakidani23@users.noreply.github.com>
2024-09-09 13:32:44 +0200

    Edit, DEL: Final function, Simulating API methods
    Simulations for api methods added, removed delete function that only removed the post locally. The new one does both locally and api delete(simulate).

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,10 +23,6 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  /// Delete post locally
-  deletePost(postId: number) {
-    this.posts = this.posts.filter((post) => post.id !== postId);
-  }
   /// Edit post locally
   editPost(post: any) {
     this.selectedPost = { ...post };
@@ -38,5 +34,36 @@ export class HomeComponent implements OnInit {
       this.posts[postIndex] = updatedPost;
     }
     this.selectedPost = null;
+  }
+
+  // Simulating API put - Also changes the postcontent on save
+  updatePost(post: any) {
+    this.http.put(`https://dummyjson.com/posts/${post.id}`, post).subscribe(
+      (response) => {
+        // Update the post in the local posts array
+        const postIndex = this.posts.findIndex((p) => p.id === post.id);
+        if (postIndex > -1) {
+          this.posts[postIndex] = response;
+        }
+        console.log('Post updated successfully:', response);
+        this.selectedPost = null; // Close the edit form after saving
+      },
+      (error) => {
+        console.log('Failed to update post:', error);
+      }
+    );
+  }
+
+  // Simulating API delete || Also deletes the Post locally
+  deletePost(postId: number) {
+    this.http.delete(`https://dummyjson.com/posts/${postId}`).subscribe(
+      (response) => {
+        this.posts = this.posts.filter((post) => post.id !== postId);
+        console.log('Post deleted successfully');
+      },
+      (error) => {
+        console.log('Failed to delete post:', error);
+      }
+    );
   }
 }
